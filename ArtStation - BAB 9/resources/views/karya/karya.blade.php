ad44dc69f80a5c5b35ee479ccb40fc6e5b52df97
yogskuyy <153170935+yogskuyy@users.noreply.github.com>
2024-06-10 09:29:09 +0700

    BAB 9-10

--- ArtStation - BAB 9/resources/views/karya/karya.blade.php
+++ ArtStation - BAB 9/resources/views/karya/karya.blade.php
@@ -1,36 +1,40 @@
 @extends('layouts.app')
 
 @section('title')
-Laporan Penjualan Karya | ArtStation
+Karya | ArtStation
 @endsection
 
 @section('content')
-<h2>Laporan Penjualan Karya</h2>
+<h2>List Penjualan Karya</h2>
 <a class="btn-print" href="{{ route('karya.add-karya') }}">Tambah Data</a>
-<a class="btn-cetak" href="{{ route('karya.download-pdf') }}">Cetak</a>
 <table id="sales-table">
-    <thead>
-        <tr>
-            <th>Photo</th>
-            <th>Nama Karya</th>
-            <th>Seniman</th>
-            <th>Harga</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($karya as $k)
-        <tr>
-            <td><img src="{{ asset('img_karya/' . $k->gambar) }}" alt="" width="300px"></td>
-            <td>{{ $k->nama_karya }}</td>
-            <td>{{ $k->seniman }}</td>
-            <td>Rp. {{ number_format($k->harga) }}</td>
-        </tr>
-        @empty
-        <tr>
-            <td colspan="4" align="center">Tidak ada data</td>
-        </tr>
-        @endforelse
-    </tbody>
+  <thead>
+    <tr>
+        <th>Photo</th>
+        <th>Nama Karya</th>
+        <th>Seniman</th>
+        <th>Harga</th>
+        <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse ($karya as $karya)
+    <tr>
+      <td><img src="{{ asset('img_karya/' . $karya->gambar)  }}" alt="" width="300px"></td>
+      <td>{{ $karya->nama_karya }}</td>
+      <td>{{ $karya->seniman }}</td>
+      <td>Rp. {{ number_format($karya->harga) }}</td>
+      <td>
+        <a class='btn-edit' href="/karya/edit/{{ $karya->id_karya }}">Edit</a>
+        <a class='btn-delete' href="/karya/hapus/{{ $karya->id_karya }}">Hapus</a>
+      </td>
+    </tr>
+    @empty
+    <tr>
+      <td colspan="5" align="center">Tidak ada data</td>
+    </tr>
+    @endforelse
+  </tbody>
 </table>
 @endsection
 
